00b1b474a40eaa617d4f6831e03e150902c343de
yassine <yassineerraoucha5@gmail.com>
2024-12-16 16:48:29 +0100

    assertj

--- src/test/java/com/hel/PersonConstructerAndGetAndSetTest.java
+++ src/test/java/com/hel/PersonConstructerAndGetAndSetTest.java
@@ -2,7 +2,7 @@ package com.hel;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-
+import static org.assertj.core.api.Assertions.*;
 import java.time.LocalDate;
 
 import org.junit.Test;
@@ -17,11 +17,19 @@ public class PersonConstructerAndGetAndSetTest {
         gis1.setFirstname("yassine");
         gis1.setLastname("erraoucha");
         gis1.setBirthdate(LocalDate.of(2003,6,27));
+        assertThat(gis1).isNotNull();
 
-      assertNotNull(gis1);
-      assertEquals("yassine",gis1.getFirstname());
-      assertEquals("erraoucha",gis1.getLastname());
-      assertEquals(LocalDate.now().getYear()-2003, gis1.calculateAge());
+        assertThat(gis1.getFirstname()).isEqualTo("yassine");  
+        assertThat(gis1.getLastname()).isEqualTo("erraoucha");
+        assertThat(gis1.getBirthdate()).isEqualTo(LocalDate.of(2003, 6, 27));
+        
+
+
+
+      // assertNotNull(gis1);
+      // assertEquals("yassine",gis1.getFirstname());
+      // assertEquals("erraoucha",gis1.getLastname());
+      // assertEquals(LocalDate.now().getYear()-2003, gis1.calculateAge());
     }
  
 
